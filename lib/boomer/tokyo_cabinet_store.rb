1ec1c630278b1b73dc069c890db0651459015c74
Jonah <jonahblochjohnson@gmail.com>
2009-02-24 21:15:29 -0500

    kadg

--- lib/boomer/tokyo_cabinet_store.rb
+++ lib/boomer/tokyo_cabinet_store.rb
@@ -1,36 +1,41 @@
 require 'tokyocabinet'
 
+ 
 module ActiveSupport
   module Cache
-    # A cache store implementation which stores data in Tokyo Cabinet:
-    # http://tokyocabinet.sourceforge.net/index.html
-    #
     class TokyoCabinetStore < Store
 
       include TokyoCabinet
       module Response # :nodoc:
-        STORED      = "STORED\r\n"
-        NOT_STORED  = "NOT_STORED\r\n"
-        EXISTS      = "EXISTS\r\n"
-        NOT_FOUND   = "NOT_FOUND\r\n"
-        DELETED     = "DELETED\r\n"
+        STORED = "STORED\r\n"
+        NOT_STORED = "NOT_STORED\r\n"
+        EXISTS = "EXISTS\r\n"
+        NOT_FOUND = "NOT_FOUND\r\n"
+        DELETED = "DELETED\r\n"
       end
 
-      attr_accessor :db_type
+			def initializer
+				@data = HDB::new
+			end
 
-      def initialize
-        @data = BDB.new
-      end
-
-      def write
-      end
-
-      def read
-      end
-
-      def delete
-      end
-
+			attr_accessor :db_type
+			
+			def write(key, value)
+			  super
+        
+			  
+			end
+			
+			def read
+			  super
+			  
+			end
+			
+			def delete
+			  super
+			  
+			end
+			
     end
   end
 end
